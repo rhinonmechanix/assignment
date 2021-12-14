fd302c650d593279b2c27cceaf3de6e9734e7094
Prabhat Patra <prabhatpatra24@gmail.com>
2021-12-13 23:33:47 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log("Hello World");
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const data = [
+    {id:1, mat:[1,2,3]}
+]
+
+app.get('/', (req,res) => {
+    res.send('Hello World')
+})
+
+app.get('/json', (req,res) => {
+    res.send(data);
+})
+
+app.post('/json', (req,res) => {
+    const json = {
+        id : data.length + 1,
+        mat : req.body.mat
+    }
+    data.push(json);
+    res.send(json)
+})
+
+app.listen(1000, () => console.log('Listening on port 1000'));
